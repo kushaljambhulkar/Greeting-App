83a7ca4094e598c24089afd13eb04386ee92cf11
Kushal Jambhulkar <kushaljambhulkar82@gmail.com>
2022-11-10 19:32:52 +0530

    update: App to delete a Greeting App to delete a Greeting

--- src/main/java/com/example/greeting/controller/GreetingController.java
+++ src/main/java/com/example/greeting/controller/GreetingController.java
@@ -17,22 +17,21 @@ public class GreetingController {
     {
         return greetingService.printMessages(greeting);
     }
-
     @GetMapping("/getMessege/{id}")
     public Optional<Greeting> findGreeting(@PathVariable int id){
         return greetingService.findGreeting(id);
     }
-
     @GetMapping(value = "/allGreeting")
     public List<Greeting> allGreeting(){
         return greetingService.allGreeting();
     }
-
     @PutMapping(value = "/update/{id}")
     public Greeting editMesseges(@PathVariable int id,@RequestParam(value = "messege") String messege,
                                  @RequestParam(value = "name") String name){
         return (greetingService.editMessages(id,messege,name));
     }
-
-   
+    @DeleteMapping(value = "/delete/{id}")
+    public void deleteGreeting(@PathVariable int id){
+        greetingService.deleteGreeting(id);
+    }
 }
