7c27692c1cd232b7d31bd2ed8c495d838f5ef061
Kushal Jambhulkar <kushaljambhulkar82@gmail.com>
2022-11-10 18:52:59 +0530

    Ability for the Greeting App to save the Greeting Message in the Repository

--- src/main/java/com/example/greeting/repository/GreetingRepository.java
+++ src/main/java/com/example/greeting/repository/GreetingRepository.java
@@ -1,11 +1,9 @@
-package com.example.greeting.repo;
+package com.example.greeting.repository;
 
 import com.example.greeting.model.Greeting;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-
 public interface GreetingRepository extends JpaRepository <Greeting,Integer> {
-
 }
